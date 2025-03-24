85d59c0ac57d13c72f599a53b03136df29969427
melanie-bauer <me.bauer2008@gmail.com>
2025-03-24 09:02:07 +0100

    fix: changes in style and console output #APM-94

--- frontend/src/components/ThemeToggle.tsx
+++ frontend/src/components/ThemeToggle.tsx
@@ -6,12 +6,11 @@ export default function ThemeToggle() {
     const {theme, setTheme} = useTheme();
     let currentTheme = theme;
     const changeTheme = () => {
-        console.log(theme);
         currentTheme = currentTheme === "light" ? "dark" : "light";
         setTheme(currentTheme);
     };
     return (
-        <Button variant="outline" size="icon" className="h-9 w-9" onClick={changeTheme}>
+        <Button variant="outline" size="icon" onClick={changeTheme}>
             <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0"/>
             <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
             <span className="sr-only">Theme wechseln</span>
