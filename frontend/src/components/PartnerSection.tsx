5f06772cb7a284deadb2eaa3549248d83d27e49d
melanie-bauer <me.bauer2008@gmail.com>
2025-03-24 18:40:03 +0100

    feat: add react component and use it #APM-58 In Progress

---
+++ frontend/src/components/PartnerSection.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import FadeIn from './FadeIn';
+
+const PartnerSection = () => {
+    return (
+        <section className="py-16 md:py-24">
+            <div className="container-xl">
+            </div>
+        </section>
+    );
+};
+
+export default PartnerSection;
