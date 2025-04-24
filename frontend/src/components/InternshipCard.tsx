771e47ab0e7dec55717749d0c3a4b3b098b69757
melanie-bauer <me.bauer2008@gmail.com>
2025-04-24 09:20:43 +0200

    fix: location for internship is not nullable #APM-103

--- frontend/src/components/InternshipCard.tsx
+++ frontend/src/components/InternshipCard.tsx
@@ -8,7 +8,7 @@ export interface InternshipProps {
     id: string;
     title: string;
     company: string;
-    location?: string;
+    location: string;
     duration: string;
     deadline: string;
     workMode: string;
@@ -90,7 +90,7 @@ const InternshipCard = ({internship, className}: InternshipCardProps) => {
             {cat}
           </span>
                 ))}
-                {internship.location && (
+                {internship.location && internship.workMode != "Remote" && (
                     <span
                         className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-emerald-100 text-emerald-800 dark:bg-emerald-900 dark:text-emerald-200">
               {internship.location}
