e9c1537c80915a445895fa8aae50393a5bf0dadb
floradallinger <flora.dallinger@gmx.at>
2025-05-01 15:30:51 +0200

    feat: added function to get a specific company from a internship. Still need to finish it

--- backend/src/router/internship-router.ts
+++ backend/src/router/internship-router.ts
@@ -26,3 +26,8 @@ internshipRouter.get("/current", async (req, res) => {
     const result = await pool.query("SELECT * FROM internship WHERE application_end > CURRENT_DATE");
     res.json(result.rows);
 });
+
+internshipRouter.get("/company", async (req, res) => {
+    const result = await pool.query("SELECT name FROM company c JOIN site s on(c.company_id = s.company_id) JOIN internship i on(s.location_id = i.location_id)");
+    res.json(result.rows);
+});
