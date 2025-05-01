90bca205f9844b150e383521dcaca0427e2fe1a8
floradallinger <flora.dallinger@gmx.at>
2025-05-01 14:58:18 +0200

    struct: added new routers and bound them to the app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,7 @@
 import {Request, Response} from 'express';
+import {internshipRouter} from "./router/internship-router";
+import {companyRouter} from "./router/company-router";
+import {studentRouter} from "./router/student-router";
 
 const express = require("express");
 const cors = require("cors");
@@ -17,34 +20,9 @@ const pool = new Pool({
 app.use(cors());
 app.use(bodyParser.json());
 
-// NOTE: temporary code, so we can see how it will look like
-// CREATE
-app.post("/items", async (req: Request, res: Response) => {
-    const { name } = req.body;
-    const result = await pool.query("INSERT INTO items (name) VALUES ($1) RETURNING *", [name]);
-    res.json(result.rows[0]);
-});
-
-// READ
-app.get("/items", async (req: Request, res: Response) => {
-    const result = await pool.query("SELECT * FROM items");
-    res.json(result.rows);
-});
-
-// UPDATE
-app.put("/items/:id", async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { name } = req.body;
-    const result = await pool.query("UPDATE items SET name = $1 WHERE id = $2 RETURNING *", [name, id]);
-    res.json(result.rows[0]);
-});
-
-// DELETE
-app.delete("/items/:id", async (req: Request, res: Response) => {
-    const { id } = req.params;
-    await pool.query("DELETE FROM items WHERE id = $1", [id]);
-    res.json({ message: "Item deleted" });
-});
+app.use("api/student", studentRouter);
+app.use("api/internship", internshipRouter);
+app.use("api/company", companyRouter);
 
 app.listen(5000, () => {
     console.log("Server running on port 5000");
