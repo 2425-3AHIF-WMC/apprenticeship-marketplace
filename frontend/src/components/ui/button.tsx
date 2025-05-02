194f7a6507bbe98f936d1e4a0168c49f5b1739b6
melanie-bauer <me.bauer2008@gmail.com>
2025-05-02 15:03:51 +0200

    style: changes in button style variant ghost

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
                     "border border-input bg-background hover:bg-accent/20 hover:text-accent-foreground",
                 secondary:
                     "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-                ghost: "hover:bg-gray-800 hover:text-accent-foreground",
+                ghost: "hover:bg-primary hover:text-primary-foreground",
                 link: "text-primary underline-offset-4 hover:underline",
             },
             size: {
