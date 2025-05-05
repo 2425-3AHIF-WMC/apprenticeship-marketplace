4d7ff9ce094e3bc494dc537202fac837ba38d4e1
llarndorfer <l.larndorfer@students.htl-leonding.ac.at>
2025-05-05 08:38:13 +0200

    style: change style of login tabs #APM-70 In Progress

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -53,16 +53,16 @@ const Login = () => {
 
                     <FadeIn delay={100}>
                         <Tabs defaultValue="student" className="w-full">
-                            <TabsList className="grid w-full grid-cols-2">
+                            <TabsList className="grid w-full grid-cols-2 border-b">
                                 <TabsTrigger
                                     value="student"
-                                    className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-900 data-[state=active]:shadow-sm flex-1"
+                                    className="flex-1 px-3 py-2.5 text-sm font-medium text-muted-foreground transition-colors duration-200 data-[state=active]:text-primary data-[state=active]:border-b-2 data-[state=active]:border-primary bg-transparent"
                                 >
                                     Schüler
                                 </TabsTrigger>
                                 <TabsTrigger
                                     value="company"
-                                    className=".text-gray-600 inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-900 data-[state=active]:shadow-sm flex-1"
+                                    className="flex-1 px-3 py-2.5 text-sm font-medium text-muted-foreground transition-colors duration-200 data-[state=active]:text-primary data-[state=active]:border-b-2 data-[state=active]:border-primary bg-transparent"
                                 >
                                     Unternehmen
                                 </TabsTrigger>
