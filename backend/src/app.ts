a7c61cb81b1f5d52019a571ad898e9e60906a095
orange-panther <katharinaeinzi@gmail.com>
2025-05-06 22:22:07 +0200

    feat: add statements to insert sample data #APM-43
    docker does not start yet

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,13 @@
 import {internshipRouter} from "./router/internship-router.js";
 import {companyRouter} from "./router/company-router.js";
 import {studentRouter} from "./router/student-router.js";
-import {ensureTablesCreated} from "./unit.js";
+import {Unit, insertSampleData, ensureTablesCreated} from "./unit.js";
 
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { Pool } from "pg";
 
 const app = express();
-const pool = new Pool({
-    user: "postgres",
-    host: "postgres",
-    database: "cruddb",
-    password: "password",
-    port: 5432,
-});
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -25,6 +17,9 @@ app.use("api/internship", internshipRouter);
 app.use("api/company", companyRouter);
 
 app.listen(5000, async () => {
+    const unit: Unit = await Unit.create(false);
+    await insertSampleData(unit);
+    await unit.complete(true);
     console.log("Server running on port 5000");
-    await ensureTablesCreated();
+    // await ensureTablesCreated(); // sollte eigentlich nicht hier passieren, sondern in der Unit
 });
