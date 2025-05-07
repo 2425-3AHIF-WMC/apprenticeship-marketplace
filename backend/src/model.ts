d05e06e89df0af366c3a8d0438f425c82ab1ad28
floradallinger <flora.dallinger@gmx.at>
2025-05-07 17:07:02 +0200

    feat: started with the internship endpoints #APM-125

--- backend/src/model.ts
+++ backend/src/model.ts
@@ -6,3 +6,18 @@ export interface IStudent{
     added: Date;
     personType: string;
 }
+
+export interface IInternshipDetailed{
+    department: string;
+    site: string;
+    duration: string;
+    description: string;
+    salary: number;
+    application_end: Date;
+    min_year: number;
+    work_type: string;
+    title: string;
+    company_name: string;
+    company_info: string;
+    company_logo: string;
+}
---
+++ backend/src/services/internship-service.ts
@@ -0,0 +1,19 @@
+import {ServiceBase} from "./service-base.js";
+import {Unit} from '../unit.js';
+import {IStudent} from "../model.js";
+
+export class StudentService extends ServiceBase{
+    constructor(unit: Unit) {
+        super(unit);
+    }
+
+    public async getAll(): Promise<IStudent[]> {
+        const stmt = await this.unit.prepare("select * from internship");
+        return stmt.rows as IStudent[];
+    }
+
+    public async getById(id: number): Promise<IStudent>{
+        const stmt = await this.unit.prepare("select * from student where student_id = ?", [id]);
+        return stmt.rows[0] as IStudent;
+    }
+}
