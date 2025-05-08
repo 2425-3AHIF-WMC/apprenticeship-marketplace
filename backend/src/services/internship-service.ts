f863764c4b547b11e408f3da3434a8e0b9845946
floradallinger <flora.dallinger@gmx.at>
2025-05-08 09:14:57 +0200

    feat: implemented the whole sql statement #APM-121 In Progess

--- backend/src/services/internship-service.ts
+++ backend/src/services/internship-service.ts
@@ -1,6 +1,6 @@
 import {ServiceBase} from "./service-base.js";
 import {Unit} from '../unit.js';
-import {IStudent} from "../model.js";
+import {IInternshipDetailed, IStudent} from "../model.js";
 
 export class StudentService extends ServiceBase{
     constructor(unit: Unit) {
@@ -13,7 +13,15 @@ export class StudentService extends ServiceBase{
     }
 
     public async getById(id: number): Promise<IStudent>{
-        const stmt = await this.unit.prepare("select description as 'department', address as 'site', name as 'duration',  from student where student_id = ?", [id]);
-        return stmt.rows[0] as IStudent;
+        const stmt = await this.unit.prepare(`select d.name as "department", s.address as "site", internship_duration.description as "duration", i.description, i.salary, i.application_end, i.min_year, w.name as "work_type", i.title, c.name as "company_name", c.company_info
+                                                                    from internship i
+                                                                        join worktype w on (i.worktype_id = w.worktype_id)
+                                                                        join internship_duration on (i.internship_duration_id = internship_duration.internship_duration_id)
+                                                                        join internship_department_map idm on (i.internship_id = idm.internship_id)
+                                                                        join department d on (idm.department_id = d.department_id)
+                                                                        join site s on (s.location_id = i.location_id)
+                                                                        join company c on (s.company_id = c.company_id)
+                                                                    where i.internship_id = ?;`, [id]);
+        return stmt.rows[0] as IInternshipDetailed;
     }
 }
