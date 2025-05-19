5fb5a31d3100b99f5c7ade28083c036318f08392
melanie-bauer <me.bauer2008@gmail.com>
2025-05-19 19:49:07 +0200

    fix: update interfaces

--- frontend/src/utils/interfaces.ts
+++ frontend/src/utils/interfaces.ts
@@ -7,45 +7,6 @@ export interface Student {
     personType: string;
 }
 
-export interface InternshipDetailed {
-    department: string;
-    site: string;
-    duration: string;
-    description: string;
-    salary: number;
-    application_end: string; // ISO date string
-    min_year: number;
-    work_type: string;
-    title: string;
-    company_name: string;
-    company_info: string;
-    company_logo: string;
-}
-
-export interface Internship {
-    title: string;
-    company_name: string;
-    application_end: string; // ISO date string
-    min_year: number;
-    department: string;
-    site: string;
-    work_type: string;
-    company_logo: string;
-    duration: string;
-}
-
-export interface Company {
-    company_id: number;
-    name: string;
-    company_info: string;
-    website: string;
-    email: string;
-    phone_number: string;
-    password: string;
-    email_verified: boolean;
-    admin_verified: boolean;
-}
-
 // --- Frontend UI interfaces (for props, cards, etc.) ---
 // These may differ from backend models and are used for UI components only.
 
@@ -60,12 +21,33 @@ export interface InternshipUIProps {
     company_logo: string;
     duration: string;
     added: string;
-    clicks: number;
+    views: number;
     category: string[];
     company_link: string;
+    internship_link: string;
+}
+export interface InternshipDetailsUIProps {
+    id: string;
+    title: string;
+    company_name: string;
+    application_end: string; // ISO date string
+    min_year: string;
+    location: string;
+    work_type: string;
+    company_logo: string;
+    duration: string;
+    added: string;
+    views: number;
+    category: string[];
+    company_link: string;
+    start_date: string;
+    salary: string;
+    internship_link: string;
+    company_id: string;
+
 }
 
-export interface CompanyUIProps {
+export interface CompanyUIPropsAdmin {
     id: string;
     name: string;
     email: string;
@@ -73,4 +55,5 @@ export interface CompanyUIProps {
     phone_number: string;
     email_verified: boolean;
     admin_verified: boolean;
+    logo: string;
 } 
