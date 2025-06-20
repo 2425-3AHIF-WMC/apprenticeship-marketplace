bd13714c0855a4f5519dcd859603ccc6e3e71dea
floradallinger <flora.dallinger@gmx.at>
2025-06-20 07:56:19 +0200

    fix: someone pushed a merge conflict (?!) fixed it

--- backend/src/services/internship-service.ts
+++ backend/src/services/internship-service.ts
@@ -83,23 +83,6 @@ export class InternshipService extends ServiceBase{
     }
 
     public async getById(id: number): Promise<IInternshipDetailsUIProps>{
-<<<<<<< HEAD
-        const stmt = await this.unit.prepare(`select i.internship_id, i.title, c.name as "company_name", i.application_end, i.min_year, s.city || ', ' || s.address  as location,
-                                                                         w.name as "work_type", c.company_logo_path, id.description as "duration", i.internship_creation_timestamp as "added", i.pdf_path as "pdf", s.location_id as "location_id",
-                                                                         (select count(*)
-                                                                          from viewed_internships vi
-                                                                          where vi.internship_id = i.internship_id) as "views", ARRAY_REMOVE(ARRAY_AGG(DISTINCT d.name), NULL) AS category,
-                                                                         c.website as "company_link", i.salary, i.internship_application_link as "internship_link", c.company_id, c.company_info
-                                                                  from internship i
-                                                                           join site s on (i.location_id = s.location_id)
-                                                                           join company c on (s.company_id = c.company_id)
-                                                                           join worktype w on (i.worktype_id = w.worktype_id)
-                                                                           join internship_duration id on (i.internship_duration_id = id.internship_duration_id)
-                                                                           left join internship_department_map idm on (i.internship_id = idm.internship_id)
-                                                                           left join department d ON d.department_id = idm.department_id
-                                                                  where i.internship_id = $1
-                                                                  group by i.internship_id, i.title, c.name, i.application_end, i.min_year, location, w.name, c.company_logo_path, id.description, i.internship_creation_timestamp, c.website, i.salary, i.internship_application_link, c.company_id, c.company_info, s.location_id;
-=======
         const stmt = await this.unit.prepare(`SELECT
                                                                     i.internship_id, 
                                                                     i.title, c.name                                 AS "company_name", 
@@ -128,7 +111,6 @@ export class InternshipService extends ServiceBase{
                                                                            LEFT JOIN department d                   ON d.department_id = idm.department_id
                                                                   WHERE i.internship_id = $1
                                                                   GROUP BY i.internship_id, i.title, c.name, i.application_end, i.min_year, location, w.name, c.company_logo_path, id.description, i.internship_creation_timestamp, c.website, i.salary, i.internship_application_link, c.company_id, c.company_info;
->>>>>>> bc5f60ef9a8a28845c4216720ae546aa2a20cfa3
                         `, [id]);
         return stmt.rows[0] as IInternshipDetailsUIProps;
     }
